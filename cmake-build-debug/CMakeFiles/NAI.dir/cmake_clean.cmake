0b445eb43d3f4cdeb4cf4579fbf6668d9df24ffc
DustEyezz <75487794+DustEyezz@users.noreply.github.com>
2022-10-06 01:05:40 +0200

    change cmake version to a more sensible 3.21

---
+++ cmake-build-debug/CMakeFiles/NAI.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NAI.dir/main.cpp.obj"
+  "CMakeFiles/NAI.dir/main.cpp.obj.d"
+  "NAI.exe"
+  "NAI.exe.manifest"
+  "NAI.pdb"
+  "libNAI.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NAI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
