716b76d4678d4ff920b3536aa4227b6a776f2629
DustEyezz <75487794+DustEyezz@users.noreply.github.com>
2022-10-17 21:55:39 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,57 +1,61 @@
 #include <iostream>
-#include <vector>
 #include <functional>
+#include <math.h>
 #include <random>
-/**
- * domain - generate domain points. Throws exception when all the points were returned
- */
-auto brute_force = [](auto f, auto domain) {
-    auto current_p = domain();
-    auto best_point = current_p;
-    try {
-        while (true) {
-            if (f(current_p) < f(best_point)) {
-                best_point = current_p;
-            }
-            current_p = domain();
-        }
-    } catch (std::exception &e) {
-    }
-    return best_point;
-};
+#include <time.h>
+using namespace std;
 using domain_t = std::vector<double>;
 std::random_device rd;
 std::mt19937 mt_generator(rd());
-domain_t hill_climbing(const std::function<double(domain_t)> &f, domain_t minimal_d, domain_t maximal_d, int max_iterations) {
-    domain_t current_p(minimal_d.size());
-    for (int i = 0; i < minimal_d.size(); i++) {
-        std::uniform_real_distribution<double> dist(minimal_d[i], maximal_d[i]);
-        current_p[i] = dist(mt_generator);
-    }
-    for (int iteration = 0; iteration < max_iterations; iteration++) {
-        domain_t new_p(minimal_d.size());
-        for (int i = 0; i < minimal_d.size(); i++) {
-            std::uniform_real_distribution<double> dist(-1.0/128.0, 1.0/128.0);
-            new_p[i] = current_p[i] + dist(mt_generator);
+
+double optimize(auto function, auto domain, int maxIterations=1000){
+    clock_t startClock, endClock;
+    startClock = clock();
+    double time;
+    uniform_real_distribution<double> dist(domain.at(0), domain.at(1));
+    double currentBest = function(domain.at(0), domain.at(1));
+
+    for(int i = 0; i < maxIterations - 1; i++){
+        double rand1 = dist(mt_generator);
+        double rand2 = dist(mt_generator);
+        double temp;
+        temp = function(rand1,rand2);
+
+        if(temp < currentBest){
+            currentBest = temp;
         }
-        if (f(current_p) > f(new_p)) {
-            current_p = new_p;
-        }
     }
-    return current_p;
+
+    endClock = clock();
+    time = ((double) (endClock - startClock)) / CLOCKS_PER_SEC;
+    cout<<"Iterations: "<<maxIterations<<" Time needed: "<<time<<" Result: ";
+    return currentBest;
 }
-int main() {
-    auto sphere_f = [](double x) {return x*x;};
-    double current_sphere_x = -10;
-    auto sphere_generator = [&]() {
-        current_sphere_x+= 1.0/128.0;
-        if (current_sphere_x >= 10) throw std::invalid_argument("finished");
-        return current_sphere_x;
+
+int main(){
+
+    //map<string, function <double (double x, double y)>> functionMap;
+
+    auto beal = [](double x, double y) {
+        return (pow((1.5 - x + (x * y)),2)) + (pow(2.25 - x + (x * pow(y,2)),2))+(pow(2.625 - x + x * pow(y,3),2));
     };
-    auto best_point = brute_force(sphere_f, sphere_generator);
-    std::cout << "best x = " << best_point << std::endl;
-    auto sphere_f_v = [](domain_t x) {return x[0]*x[0];};
-    auto best2 = hill_climbing(sphere_f_v, {-10},{10},10000);
-    std::cout << "best x = " << best2[0] << std::endl;
+
+    auto himmel = [](double x, double y) {
+        return pow(pow(x,2) + y - 11,2) + pow(x + pow(y,2) - 7,2);
+    };
+
+    auto threeHumpCamel = [](double x, double y) {
+        return ((2 * pow(x,2)) - (1.05 * pow(x,4)) + ((pow(x,6))/6) + (x*y) + (pow(y,2)));
+    };
+
+    auto matyas = [](double x, double y) {
+        return (0.26 * (pow(x, 2) + pow(y, 2)) - (0.48 * x * y));
+    };
+
+    vector<double> domain = {-10,10};
+
+    for(int i = 0; i < 10; i++){
+        cout<<optimize(matyas,domain,10000)<<endl;
+    }
     return 0;
 }
