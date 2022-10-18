32e733701d84263600a0aa9659848735dd105208
DustEyezz <75487794+DustEyezz@users.noreply.github.com>
2022-10-19 01:05:37 +0200

    added gnuplot and suffered a lot + messed up my sleep schedule

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@ std::mt19937 mt_generator(rd());
 
 
 double optimize(myfunction_t function, vector<double> domain, int maxIterations=10000){
+
+    FILE *fp = NULL;
+    fp = fopen("data.txt", "a");
+
     auto start = std::chrono::high_resolution_clock::now();
     uniform_real_distribution<double> dist(domain.at(0), domain.at(1));
     double currentBest = function(domain.at(0), domain.at(1));
@@ -28,11 +32,15 @@ double optimize(myfunction_t function, vector<double> domain, int maxIterations=
     }
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    fprintf(fp, "%f %f\n", (double)duration.count(), currentBest);
     cout<<"Iterations: "<<maxIterations<<" Time taken: "<<duration.count()<<" microseconds Lowest: ";
     return currentBest;
 }
 
 int main(int argc, char **argv){
+    FILE *gnupipe = NULL;
+    char *gnuCommands [] = {"set title \"Optimization function graph\"", "f(x) = mean_y", "fit f(x) 'data.txt' via mean_y", "set xlabel\"time in microseconds\"", "set ylabel \"lowest generated\"", "plot 'data.txt', \ f(x)"};
+    gnupipe = _popen("gnuplot -persistent", "w");
 
     map<string, myfunction_t> myFunctions;
     map<string, vector<double>> domain;
@@ -55,10 +63,13 @@ int main(int argc, char **argv){
    domain["threeHumpCamel"] = {-5,5};
    domain["matyas"] = {-10,10};
 
+   if( remove( "data.txt" ) != 0 )
+       cout << "Creating file\n";
+
    try {
        vector<string> arguments(argv, argv + argc);
        auto selectedFunction = arguments.at(1);
-       for(int i = 0; i < 10; i++){
+       for(int i = 0; i < 20; i++){
            cout<<optimize(myFunctions.at(selectedFunction),domain.at(selectedFunction),10000)<<endl;
        }
    }
@@ -68,5 +79,8 @@ int main(int argc, char **argv){
        cout << endl;
        return 1;
    }
+    for (int i = 0; i < 7; i++) {
+        fprintf(gnupipe, "%s\n", gnuCommands[i]);
+    }
    return 0;
 }
