9a036d2a67130f3dc4287e51cbc88d253e9422a6
DustEyezz <75487794+DustEyezz@users.noreply.github.com>
2022-11-01 19:20:07 +0100

    working on fitness

--- main.cpp
+++ main.cpp
@@ -12,8 +12,20 @@ std::mt19937 mt_generator(rd());
 using chromosome_t = std::vector<int>;
 using population_t = std::vector<chromosome_t>;
 
+population_t populate(int pop_size, int chrom_size){
+    srand(time(nullptr));
+    population_t population;
+    for(int i=0;i<pop_size;i++){
+        chromosome_t chromosome;
+        for(int j=0;j<chrom_size;j++){
+            chromosome.push_back(rand()%2);
+        }
+        population.push_back(chromosome);
+    }
+    return population;
+}
+
 pair<double, double> translate(chromosome_t chromosome){
-    using namespace std;
     pair<double, double> result={0,0};
     bool flagNegativeX = false;
     if(chromosome.at(0)==1)flagNegativeX=true;
@@ -21,14 +33,17 @@ pair<double, double> translate(chromosome_t chromosome){
     if(chromosome.at(1)==1)flagNegativeY=true;
 
     int splitpoint = chromosome.size()/2;
+    //cout << splitpoint << endl;
 
-
-    
-    for(int i=splitpoint;i>=3;i--){
-        result.first+=(chromosome.at(i)*pow(2,i));
+    int j = 0;
+    for(int i=splitpoint;i>1;i--){
+        result.first += (chromosome.at(i) * pow(2,j));
+        j++;
     }
-    for (int i = chromosome.size(); i < splitpoint; i--) {
-        result.second += (chromosome.at(i)*pow(2,i));
+    j=0;
+    for (int i = chromosome.size()-1; i > splitpoint; i--) {
+        result.second += (chromosome.at(i) * pow(2,j));
+        j++;
     }
 
     if(flagNegativeX){
@@ -37,17 +52,25 @@ pair<double, double> translate(chromosome_t chromosome){
     if(flagNegativeY){
         result.second*=-1;
     }
+    result.first = result.first/100000000000000;
+    result.second = result.second/100000000000000;
     return result;
 }
 
 auto genetic_algorithm = [](
         auto initial_population, auto fitness, auto term_condition,
-        auto selection, double p_crossover,
-        auto crossover, double p_mutation,  auto mutation){
+        auto selection,
+        double p_crossover,
+        auto crossover,
+        double p_mutation,
+        auto mutation,
+        myfunction_t function,
+        vector<double> domain,
+        double goal){
     using namespace std;
     uniform_real_distribution<double> uniform(0.0,1.0);
     auto population = initial_population;
-    vector<double> population_fit = fitness(population);
+    vector<double> population_fit = fitness(population, function, domain, goal);
     while (!term_condition(population,population_fit)) {
         auto parents_indexes = selection(population_fit);
         decltype(population) new_population;
@@ -62,12 +85,24 @@ auto genetic_algorithm = [](
             chromosome = mutation(chromosome,p_mutation);
         }
         population = new_population;
-        population_fit = fitness(population);
+        population_fit = fitness(population, function, domain, goal);
     }
     return population;
 };
-std::vector<double> fitness_function(population_t pop){
-    return {};
+std::vector<double> fitness_function(population_t pop, myfunction_t function, vector<double> domain, double goal){
+    vector<double> result;
+    pair <double, double> currPair;
+    for (int i = 0; i < pop.size(); i++){
+        currPair = translate(pop.at(i));
+        if (currPair.first > domain.at(0) && currPair.first < domain.at(1) && currPair.second > domain.at(0) && currPair.second < domain.at(1)){
+            result.push_back(function(currPair));
+        }
+        else {
+
+        }
+
+    }
+    return result;
 }
 std::vector<int> selection_empty(std::vector<double> fitnesses) {
     return {};
@@ -80,12 +115,10 @@ chromosome_t mutation_empty(chromosome_t parents, double p_mutation) {
 }
 
 int main(int argc, char **argv){
-    //FILE *gnupipe = NULL;
-    //char *gnuCommands [] = {"set title \"Optimization function graph\"", "f(xy.front()) = mean_y", "g(xy.back()) = mean_x", "fit f(xy.front()) 'data.txt' using 1:2 via mean_y", "fit g(xy.front()) 'data.txt' using 2:1 via mean_x", "set xlabel\"time in microseconds\"", "set ylabel \"lowest generated\"", "plot 'data.txt', \ f(xy.front()), \ g(xy.front())"};
-    //gnupipe = _popen("gnuplot -persistent", "w");
 
     map<string, myfunction_t> myFunctions;
     map<string, vector<double>> domain;
+    map<string, double> goal;
 
     myFunctions["beale"] = [](pair<double, double> xy) {
         return (pow((1.5 - xy.first + (xy.first * xy.second)),2)) + (pow(2.25 - xy.first + (xy.first * pow(xy.second,2)),2))+(pow(2.625 - xy.first + xy.first * pow(xy.second,3),2));
@@ -100,42 +133,44 @@ int main(int argc, char **argv){
         return (0.26 * (pow(xy.first, 2) + pow(xy.second, 2)) - (0.48 * xy.first * xy.second));
     };
 
-   domain["beale"] = {-4.5,4.5};
-   domain["himmel"] = {-5,5};
-   domain["threeHumpCamel"] = {-5,5};
-   domain["matyas"] = {-10,10};
+    domain["beale"] = {-4.5,4.5};
+    domain["himmel"] = {-5,5};
+    domain["threeHumpCamel"] = {-5,5};
+    domain["matyas"] = {-10,10};
 
-   population_t population = {{1,0,1,0,1,0,1}, {1,0,1,0,1,0,1}};
+    goal["beale"] = 0;
+    goal["himmel"] = 0;
+    goal["threeHumpCamel"] = 0;
+    goal["matyas"] = 0;
 
-   auto result = genetic_algorithm(population,
-                                   fitness_function,
-                                   [](auto a, auto b) { return true; },
-                                   selection_empty,
-                                   1.0,
-                                   crossover_empty,
-                                   0.01,
-                                   mutation_empty);
-   for (chromosome_t chromosome: result) {
-        cout << "[";
-        for (int p: chromosome) {
-            cout << p;
-        }
-        cout << "] ";
-   }
-   cout << endl;
+   population_t population = populate(100, 100+(23316%10)*2);
+   pair<double, double> test = translate(population.at(1));
 
-/*
+   printf("%f %f", test.first, test.second);
+
+
    try {
        vector<string> arguments(argv, argv + argc);
        auto selectedFunction = arguments.at(1);
-       for (int i = 0; i < 25; i++) {
-           cout << "BruteForce: ";
-           cout << bruteForce(myFunctions.at(selectedFunction), domain.at(selectedFunction), 10000) << endl;
-           cout << "HillClimbing: ";
-           cout << hillClimbing(myFunctions.at(selectedFunction), domain.at(selectedFunction), 10000) << endl;
-           cout << "Simulated Annealing: ";
-           cout << simulatedAnnealing(myFunctions.at(selectedFunction), domain.at(selectedFunction), 10000) << endl;
+       auto result = genetic_algorithm(population,
+                                       fitness_function,
+                                       [](auto a, auto b) { return true; },
+                                       selection_empty,
+                                       1.0,
+                                       crossover_empty,
+                                       0.01,
+                                       mutation_empty,
+                                       myFunctions.at(selectedFunction),
+                                       domain.at(selectedFunction),
+                                       goal.at(selectedFunction));
+       for (chromosome_t chromosome: result) {
+           cout << "[";
+           for (int p: chromosome) {
+               cout << p;
+           }
+           cout << "] ";
        }
+       cout << endl;
    }
    catch (std::out_of_range aor) {
        cout << "Blad. Podaj poprawny argument. Dostepne to: ";
@@ -143,9 +178,6 @@ int main(int argc, char **argv){
        cout << endl;
        return 1;
    }
-   */
-    //for (int i = 0; i < 8; i++) {
-    //    fprintf(gnupipe, "%s\n", gnuCommands[i]);
-    //}
+
    return 0;
 }
