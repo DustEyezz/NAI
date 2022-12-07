114b6b11191a0498870397435fe5924d97d5048e
DustEyezz <75487794+DustEyezz@users.noreply.github.com>
2022-12-07 16:30:45 +0100

    need to finish this

--- main.cpp
+++ main.cpp
@@ -1,24 +1,54 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
-
-int main(int argc, char** argv)
+#include <vector>
+// g++ `pkg-config --cflags opencv4` cv2.cpp `pkg-config --libs opencv4`
+using namespace std;
+using namespace cv;
+int main(int argc, char **argv)
 {
-    using namespace cv;
-    VideoCapture vc(0);
-    if (!vc.isOpened() ) return 1;
-    cv::Mat frame;
-    cv::Mat myFrame;
+    std::vector<int> lower = {0,99,239};
+    std::vector<int> upper = {255,255,255};
+    VideoCapture cap1(0);
+    if (!cap1.isOpened())
+        return -1;
+    //namedWindow("pierwsze", WINDOW_AUTOSIZE);
+    namedWindow("detected", WINDOW_AUTOSIZE);
+//    createTrackbar("lh", "detected", &lower[0], 255);
+//    createTrackbar("ls", "detected", &lower[1], 255);
+//    createTrackbar("lv", "detected", &lower[2], 255);
+//    createTrackbar("hh", "detected", &upper[0], 255);
+//    createTrackbar("hs", "detected", &upper[1], 255);
+//    createTrackbar("hv", "detected", &upper[2], 255);
+    while (true)
+    {
+        Mat f1, dst, detected,dilated;
+        cap1.read(f1);
+        cvtColor(f1, dst, COLOR_BGR2HSV);
 
-    cv::namedWindow("Window", WINDOW_AUTOSIZE); // Create a window for display.
-    int i = 0;
-    while (waitKey(10) != 27) {
-        vc >> frame;
-        std::cout << "frame info: " << frame.rows << " " << frame.cols << std::endl;
-        //imwrite("frame" + std::to_string(i++) + ".png", frame);
-        cv::flip(frame, myFrame, 1);
-        cv::imshow("Frame", myFrame);
-
+        //imshow("pierwsze", f1);
+        //imshow("wynik", dst);
+        inRange(dst, lower,upper, detected);
+        auto kernel = getStructuringElement(MORPH_ELLIPSE,Size{5,5});
+        erode(detected, dilated, kernel);
+        dilate(dilated, dilated, kernel);
+        //imshow("dilated", dilated);
+        vector<vector<Size>> contours;
+        vector<Vec4i> hierarchy;
+        findContours (dilated, contours, hierarchy, RETR_LIST, CHAIN_APPROX_SIMPLE);
+        //cout << contours.size() << endl;
+        auto selected = *std::max_element(contours.begin(), contours.end(), [](auto a, auto b){
+            return contourArea(a) < contourArea(b);});
+        std::sort(contours.begin(), contours.end(), selected());
+        //cout << " " << selected[0] << endl;
+        if(selected.size()>46){
+            cv::line(dst, selected.at(0), selected.at(40), {255,0,0}, 2);
+            cv::line(dst, selected.at(5), selected.at(45), {255,0,0}, 2);
+        }
+        imshow("wynik", dst);
+        imshow("detected", dilated);
+        if (waitKey(1) == 27)
+            break;
     }
     return 0;
 }
